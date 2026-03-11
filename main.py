9cda5d788e3798fbf42f18afe39bbbcf699f4976
AntonMitkov <mitkovanton1@gmail.com>
2026-03-11 10:33:02 +0300

    2 commit

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ app = FastAPI()
 @app.get('/')
 def base():
     return {
-        'Василевская Алина': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/aline.JPG'},
-        'Коновалова Настя': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/nasya.png'},
-        'Антонова Ксюша': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/ksusha.jpg'},
-        'Сидоренко Эмилия': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/emily.png'},
-        'Самокиш Ангелина': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/angie.png'},
-        'Шаповалова Адель': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/adely.png'},
-        'Искорцева Полина': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/poline.jpg'},
+        'Василевская Алина': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/aline.JPG', 'link': 'https://t.me/miss_lyceum_lbsu_bot?startapp'},
+        'Коновалова Настя': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/nasya.png', 'link': 'https://t.me/miss_lyceum_lbsu_bot?startapp=nastya'},
+        'Антонова Ксюша': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/ksusha.jpg', 'link': 'https://t.me/miss_lyceum_lbsu_bot?startapp=nastya'},
+        'Сидоренко Эмилия': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/emily.png', 'link': 'https://t.me/miss_lyceum_lbsu_bot?startapp=nastya'},
+        'Самокиш Ангелина': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/angie.png', 'link': 'https://t.me/miss_lyceum_lbsu_bot?startapp=nastya'},
+        'Шаповалова Адель': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/adely.png', 'link': 'https://t.me/miss_lyceum_lbsu_bot?startapp=nastya'},
+        'Искорцева Полина': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/poline.jpg', 'link': 'https://t.me/miss_lyceum_lbsu_bot?startapp=nastya'},
         'Ляшевич Соня': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/sonya.jpg'},
     }
