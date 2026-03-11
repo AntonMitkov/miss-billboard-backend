97b3b2edce069662b09653e4b153f180e0dbaf06
AntonMitkov <mitkovanton1@gmail.com>
2026-03-11 10:41:00 +0300

    link fix

--- main.py
+++ main.py
@@ -12,5 +12,5 @@ def base():
         'Самокиш Ангелина': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/angie.png', 'link': 'https://t.me/miss_lyceum_lbsu_bot?startapp=nastya'},
         'Шаповалова Адель': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/adely.png', 'link': 'https://t.me/miss_lyceum_lbsu_bot?startapp=nastya'},
         'Искорцева Полина': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/poline.jpg', 'link': 'https://t.me/miss_lyceum_lbsu_bot?startapp=nastya'},
-        'Ляшевич Соня': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/sonya.jpg'},
+        'Ляшевич Соня': {'src': 'https://jhtcwaillrfwthaxukyy.supabase.co/storage/v1/object/public/miss_photos/sonya.jpg', 'link': 'https://t.me/miss_lyceum_lbsu_bot?startapp=nastya'},
     }
